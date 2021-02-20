4c9c0d15e50f5c5e1ef51d7d346820b793497f84
kubot080301 <70013856+kubot080301@users.noreply.github.com>
2021-02-20 10:16:16 +0800

    first update
    include lib 進行c、c++混用專案
    建立class
    建立指標

--- Core/KUBOT/ledcpp.h
+++ Core/KUBOT/ledcpp.h
@@ -32,15 +32,13 @@
 class CLed
 {
   GPIO_TypeDef* _port;                                      //GPIOA,GPIOB....
-  uint16_t _pin;                                            //GPIO_PIN_0,GPIO_PIN_1....
-  uint16_t _toggleTime;                                     //Toggle time in ms
-  uint16_t counter;                                         //Toggle time counter
-  void on(){HAL_GPIO_WritePin(_port,_pin,GPIO_PIN_SET);}    //turn on LED
-  void off(){HAL_GPIO_WritePin(_port,_pin,GPIO_PIN_RESET);} //turn off LED
-  void toggle(){HAL_GPIO_TogglePin(_port,_pin);}            //toggle LED
+  uint16_t _pin;                                            //GPIO_PIN_0,GPIO_PIN_1....                                        //Toggle time counter
+	
 public:
-  CLed(GPIO_TypeDef* port,uint16_t pin,uint16_t toggleTim); //constructor
-  void runToggle();                                         //run toggling LED from system tick every 1ms	
+  CLed(GPIO_TypeDef* port,uint16_t pin); //constructor
+	void on();																								//turn on LED
+	void off();																								//turn off LED
+	void toggle();																						//toggle
 };
 
 #endif
