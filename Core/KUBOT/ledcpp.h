e7617a9ce25a4f07da625e43e7b5fb17a312fbba
kubot080301 <70013856+kubot080301@users.noreply.github.com>
2021-02-20 11:20:46 +0800

    update

--- Core/KUBOT/ledcpp.h
+++ Core/KUBOT/ledcpp.h
@@ -1,30 +1,3 @@
-/**
- * @file LEDcpp.hpp
- * Head file of CLED class
- * Jast for STM32Cube C++ programming example
- * Keil ARM MDK V5.05
- * STM32 F4 Discovery board (STM32F407VGT6)
- * STM32CubeMX V4.9.0
- *
- * Copyright (C) Ian Jin
- * iancanada.mail@gmail.com
- * https://github.com/iancanada/
- * Twitter: @iancanadaTT
- *
- * This program is a free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or any later
- * version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef _LEDCPP_H
 #define _LEDCPP_H
 #include "stm32f4xx_hal.h"
